7be2aa7bd9fcebfbc6c774ccc910f5aeffb1d940
The-Lunacy-Destroyer <golubevivan0500@gmail.com>
2025-09-18 20:50:34 +0300

    Add project template files

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace mathgame.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace mathgame.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
