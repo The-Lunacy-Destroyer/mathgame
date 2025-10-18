a431b431dc3c1bb54f43b53db7d0f6e2740e74cc
The-Lunacy-Destroyer <golubevivan0500@gmail.com>
2025-10-19 01:38:35 +0300

    Add basic player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rb = null;
+    private Camera _mainCamera = null;
+
+    public float speed = 1f;
+    public float maxSpeed = 10f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private Vector3 GetPlayerToMouseVector()
+    {
+        Vector3 mousepos = _mainCamera.ScreenToWorldPoint(Mouse.current.position.value);
+        return mousepos - transform.position;
+    }
+
+    private void MovePlayer()
+    {
+        if (Mouse.current.leftButton.isPressed)
+        {
+            Vector2 direction = GetPlayerToMouseVector().normalized;
+            Vector2 movement = direction * (Time.fixedDeltaTime * 100f * speed);
+            _rb.AddForce(movement);
+            _rb.transform.up = direction;
+        }
+        if (_rb.linearVelocity.magnitude > maxSpeed)
+        {
+            _rb.linearVelocity = _rb.linearVelocity.normalized * maxSpeed;
+        }
+    }
+}
