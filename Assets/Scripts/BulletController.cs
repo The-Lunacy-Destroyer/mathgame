e995013f440eb4064be5d9d12050a2b08a9df04b
zxcvbnm269 <fhhdhtdhcf@gmail.com>
2025-10-21 21:56:01 +0300

    Add BulletController and Launch method

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class BulletController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.magnitude > 100.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+    }
+}
