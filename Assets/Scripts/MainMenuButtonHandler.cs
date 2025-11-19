49f826aaa1cf86df873df69b49202d0b9439c746
The-Lunacy-Destroyer <golubevivan0500@gmail.com>
2025-11-19 13:19:56 +0300

    Add main menu with newgame and leave buttons

---
+++ Assets/Scripts/MainMenuButtonHandler.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class MainMenuButtonHandler : MonoBehaviour
+{
+    private VisualElement _root;
+    
+    private Button _newGameButton;
+    private Button _leaveButton;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnEnable()
+    {
+        _root = GetComponent<UIDocument>().rootVisualElement;
+        
+        _newGameButton = _root.Q<Button>("NewGame");
+        _leaveButton = _root.Q<Button>("Leave");
+        
+        _newGameButton.clicked += NewGame;
+        _leaveButton.clicked += Leave;
+    }
+
+    private void OnDisable()
+    {
+        _newGameButton.clicked -= NewGame;
+        _leaveButton.clicked -= Leave;
+    }
+
+    private void NewGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    private void Leave()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
