b1f48194f9bf94f1422fdb0aa3b272efc99a6404
zxcvbnm269 <fhhdhtdhcf@gmail.com>
2025-12-01 11:11:14 +0300

    Fix guns

--- Assets/Scripts/MinibossController.cs
+++ Assets/Scripts/MinibossController.cs
@@ -18,12 +18,14 @@ public class MinibossController : EntityController
     }
     private void FixedUpdate()
     {
-        Debug.Log(Gun1.rotation);
-        if (_canLaunchProjectile)
-        {
-            LaunchProjectile(Gun1.position, new Vector2(0, 1), false);
-            LaunchProjectile(Gun2.position, new Vector2(1, -1), true);
-            LaunchProjectile(Gun3.position, new Vector2(-1, -1), true);
+        Vector2 vec1 = (Gun1.position - Center.position).normalized;
+        Vector2 vec2 = (Gun2.position - Center.position).normalized;
+        Vector2 vec3 = (Gun3.position - Center.position).normalized;
+        if (_canLaunchProjectile) { 
+        
+            LaunchProjectile(Gun1.position, vec1, false);
+            LaunchProjectile(Gun2.position, vec2, true);
+            LaunchProjectile(Gun3.position, vec3, true);
         }
         
     }
