c63764bf9725a18b2f95f24705fcf35771d9ec99
zxcvbnm269 <fhhdhtdhcf@gmail.com>
2025-12-15 00:16:04 +0300

    Add meteor health and background meteors script

--- Assets/Scripts/Meteors/MeteorsController.cs
+++ Assets/Scripts/Meteors/MeteorsController.cs
@@ -2,46 +2,30 @@ using Health;
 using Projectile;
 using UnityEngine;
 
-public class MeteorsController : MonoBehaviour
+public class MeteorsController : EntityController
 {
     Rigidbody2D rb;
-    Vector2 moveDirection;
     float rand1;
     float rand2;
-    float force = 10f;
     float slowdown = 1f;
-    public bool is_background_meteor = false;
 
     void Start()
     {
         rand1 = Random.Range(-1.0f, 1f);
-        rand2 = Random.Range(-1.0f, 1f);
         rb = GetComponent<Rigidbody2D>();
-        moveDirection = new Vector2(rand1, rand2);
     }
 
     void FixedUpdate()
     {
-        if (is_background_meteor) slowdown = 0.5f;
-        rb.position += moveDirection * 0.05f * slowdown;
         rb.transform.up = Utilities.MathUtilities.RotateVector(rb.transform.up, (rand1 + Mathf.Sign(rand1) * 0.5f) * slowdown);
-        float x = rb.position.x;
-        float y = rb.position.y;
-        if (x < -74 || x > 71 || y < -46 || y > 50)
-        {
-            float new_x = -1.5f - x;
-            float new_y = 2f - y;
-            rb.position = new Vector2(new_x, new_y);
-        }
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
         MonoBehaviour otherObj = other.GetComponent<MonoBehaviour>();
         if (otherObj is ProjectileController projectile)
         {
             Rigidbody2D proj_rb = projectile.GetComponent<Rigidbody2D>();
-            rb.AddForce(proj_rb.linearVelocity * force);
             Destroy(projectile.gameObject);
         }
         else if (otherObj is EntityHealthController entity)
--- Assets/Scripts/SpawnMeteors.cs
+++ Assets/Scripts/SpawnMeteors.cs
@@ -10,9 +10,15 @@ public class SpawnMeteors : MonoBehaviour
         for(int i = 0; i < amount; i++)
         {
             Rigidbody2D meteor_rb = meteor.GetComponent<Rigidbody2D>();
+            SpriteRenderer meteor_sr = meteor.GetComponent<SpriteRenderer>();
             float random_scale = Random.Range(0.5f, 2.0f);
+            float random_mass = Random.Range(0.5f, 5.0f);
 
-            meteor_rb.mass = Random.Range(0.5f, 2.0f);
+            float v = (random_mass - 0.5f) / 4.5f;
+
+
+            meteor_rb.mass = random_mass;
+            meteor_sr.color = new Color(0.6f + v * 0.4f, 0.4f + v * 0.6f, 0.4f + v * 0.6f);
             meteor.transform.localScale = new Vector3(random_scale, random_scale, 0);
 
             Instantiate(meteor, Utilities.MathUtilities.RectRandomPos(-74f, 71f, -46f, 50f), Random.rotation);
